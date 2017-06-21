e860ad9293dfd925997b14796cbe8940d071006e
piyushawe <piyush.nautiyal@franciscansolutions.in>
2017-06-21 15:21:14 +0530

    First commit

--- src/main/Code/Runner1.java
+++ src/main/Code/Runner1.java
@@ -4,12 +4,16 @@ import com.github.mkolisnyk.cucumber.runner.AfterSuite;
 import com.github.mkolisnyk.cucumber.runner.BeforeSuite;
 import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
 import cucumber.api.CucumberOptions;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.runner.RunWith;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 @RunWith(ExtendedCucumber.class)
@@ -28,13 +32,22 @@ import java.io.FileNotFoundException;
 )
 public class Runner1 {
     @BeforeSuite
-    public static void  setup() throws FileNotFoundException {
+    public static void  setup() throws IOException {
         String loc="D:\\School_login.xlsx";
         File fl= new File(loc);
         FileInputStream in= new FileInputStream(fl);
         Workbook wb=null;
         String sr=loc.substring(loc.indexOf("."));
-        System.out.println(sr);
+        //System.out.println(sr);
+        if(sr.equals(".xls"))
+        {
+            wb= new HSSFWorkbook(in);
+        }
+        else if(sr.equals(".xlsx"))
+        {
+           wb = new XSSFWorkbook(in);
+        }
+        Sheet sh= wb.getSheetAt(0);
 
 
     }
