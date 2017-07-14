75587ba92d8762d2786c05dbfea616e6772825db
piyushawe <piyush.nautiyal@franciscansolutions.in>
2017-07-14 11:15:38 +0530

    Futher code

--- src/main/Code/Runner1.java
+++ src/main/Code/Runner1.java
@@ -53,7 +53,7 @@ public class Runner1 {
         FileInputStream in= new FileInputStream(fl);
         Workbook wb=null;
         String sr=loc.substring(loc.indexOf("."));
-        //System.out.println(sr)qwqweea;
+        //System.out.println(sr)qwqweea12;
         if(sr.equals(".xls"))
         {
             wb= new HSSFWorkbook(in);
